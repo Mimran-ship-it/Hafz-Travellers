e2654b954f4d4cfc295b57c92f21cf909c7f3ef4
Ibrahim Bajwa <129992649+IbrahimBajwa313@users.noreply.github.com>
2024-08-25 06:31:35 -0700

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,26 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
 
-import ProductDetailCrousel from '@/components/ProductDeatailCrousel'
-import RelatedProducts from '@/components/RelatedProducts'
-import Wrapper from '@/components/Wrapper'
-import React, { useState,useEffect } from 'react'
-import { IoMdHeartEmpty } from 'react-icons/io'
-import { MyContext } from '@/pages/_app'
-import { useRouter } from 'next/router'
-import AddToCartNotification from '@/components/AddToCartNotification'
-import { motion } from 'framer-motion'
-
-export default function ProductDetails() {
-const [Items, setItems] = useState([]);
-
-useEffect(() => {
+const ProductCard = ({ slug,query }) => {
+  // console.log(slug,query)
+  const [products, setProducts] = useState([]);
+  const [Category, setCategory] = useState('');
+  
+  
+// console.log('data',query.length)s
+  useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('/api/getProducts');
         const result = await response.json();
 
-        // Ensure the response has a "products" property and it's an array before setting state
         if (result && Array.isArray(result.products)) {
-          setItems(result.products);
+          setProducts(result.products);
         } else {
           console.error('Invalid data structure received:', result);
         }
@@ -32,94 +29,118 @@ useEffect(() => {
     fetchData();
   }, []);
 
-    const [isCartVisible, setIsCartVisible] = useState(false);
-    const [size, setSize] = useState(undefined)
+  useEffect(() => {
+    setCategory(slug);
+  }, [slug]);
 
-    const router = useRouter()
-    const { slug } = router.query
+  return (
+    <>
+    {query?.length==0&&<> 
+      {products.map((item) => {
+        const discountPercentage = Math.round(((item.originalPrice - item.price) / item.originalPrice) * 100);
 
-    // Using Context API to use app functions in this file
-    const { addToCart } = MyContext()
-
-
-    const [pin, setPin] = useState(undefined)
-    const [service, setService] = useState()
-
-    // Pin Code Service Availability 
-    const checkServiceAvailability = async () => {
-        let pins = await fetch("/api/pincode");
-        let pinJson = await pins.json();
-        if (pinJson.includes(+pin)) {
-            setService(true)
-        }
-        else {
-            setService(false)
-        }
-    }
-
-
-    const onChangePin = (e) => {
-        setPin(e.target.value)
-    }
-    
-    return (
-        <motion.div  initial={{ opacity: 0 ,scale:1,y:-10   }}  animate={{y:0,  opacity: 1, scale: 1}} transition={{ delay: 0,duration:.3,stiffness:50 }} className='w-full min-h-screen md:py-20'>
-            {/* {console.log('asas',Items[0]?._id)}
-            {console.log(slug)} */}
-{
-Items.map((key)=>{
-    return <>{key?._id==slug&&<div>
-       <Wrapper>
-                <div className=' flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]'>
-                    {/* left col start  */}
-                    <div className='w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0'><ProductDetailCrousel img={key.img} /> </div>
-                    {/* left col end  */}
-
-                    {/* Right col start  */}
-                    <div className="flex-[1] py-3">
-
-                        {/* Product Title */}
-                        <div className='font-bold text-[31px] mb-2'>
-                            {key.title}
-                        </div>
-
-                        {/* Product Subtitle */}
-                        <div className='text-lg font-semibold mb-5'>
-                        {key.category}
-                        </div>
-
-                        {/* Price Details */}
-                        <div className='text-lg font-semibold'>
-                            Price : {key.price} $
-                        </div>
-                        <div className='text-sm text-black/[0.5]'>
-                            Incl. of Texes
-                        </div>
-                        <div className='text-sm text-black/[0.5] mb-20'>
-                            {`(Also includes all applicable duties)`}
-                        </div>
-
-                        
-
-
-
-
-
-
-                    </div>
-                    {/* Right col end  */}
-
+        return (
+        <>  {item.category==slug?.toLowerCase()&&<motion.div
+            key={item._id}
+            initial={{ opacity: 0, scale: 1, y: -30 }}
+            animate={{ y: 0, opacity: 1, scale: 1 }}
+            transition={{ delay: 0, duration: 0.3, stiffness: 50 }}
+            className='bg-white border border-black shadow-xl duration-200 hover:scale-105 cursor-pointer '
+          > 
+            <Link href={`/product/${item._id}`}>
+              <Image
+                src={`/productIamages/${item.img}/thumbnail.jpg`}
+                alt="Product-Image"
+                width={400}
+                height={400}
+                className='transition-transform duration-300 hover:scale-105'
+              />
+              <div className='p-4 text-black-[0.9]'>
+                <h2 className='text-lg font-medium'>{item.title}</h2>
+                <div className='flex items-center text-black-[0.8]'>
+<<<<<<< HEAD
+                  <p className='mr-2 text-lg font-semibold'>{item.price} /- Rs</p>
+                  <p className='text-base font-medium line-through'>{item.originalPrice} /- Rs</p>
+=======
+                  <p className='mr-2 text-lg font-semibold'>{item.price}$</p>
+                  <p className='text-base font-medium line-through'>{item.originalPrice}$</p>
+>>>>>>> 8009f0b54fe0e71de6de1e6e0856c4a0ec9f0011
+                  <p className='ml-auto text-green-300 font-base'>{discountPercentage}% off</p>
                 </div>
-             
+              </div>
+            </Link>
+          </motion.div>
+        }</>);
+      })}
+    </>}
+    {query==undefined&&slug==undefined&&<> 
+      {products.map((item) => {
+        const discountPercentage = Math.round(((item.originalPrice - item.price) / item.originalPrice) * 100);
 
-                <RelatedProducts />
-            </Wrapper>
+        return (
+        <>  <motion.div
+            key={item._id}
+            initial={{ opacity: 0, scale: 1, y: -30 }}
+            animate={{ y: 0, opacity: 1, scale: 1 }}
+            transition={{ delay: 0, duration: 0.3, stiffness: 50 }}
+            className='bg-white shadow-xl duration-200 hover:scale-105 cursor-pointer '
+          >
+            <Link href={`/product/${item._id}`}>
+              <Image
+                src={`/productIamages/${item.img}/thumbnail.jpg`}
+                alt="Product-Image"
+                width={400}
+                height={400}
+                className='transition-transform duration-300 hover:scale-105 '
+              />
+              <div className='p-4 text-black-[0.9]'>
+                <h2 className='text-lg font-medium'>{item.title}</h2>
+                <div className='flex items-center text-black-[0.8]'>
+                  <p className='mr-2 text-lg font-semibold'>{item.price}/- Rs</p>
+                  <p className='text-base font-medium line-through'>{item.originalPrice}</p>
+                  <p className='ml-auto text-green-300 font-base'>{discountPercentage}% off</p>
+                </div>
+              </div>
+            </Link>
+          </motion.div>
+        </>);
+      })}
+    </>}
+    {query?.length!=0&&<>
+      {query?.map((item) => {
+        const discountPercentage = Math.round(((item.originalPrice - item.price) / item.originalPrice) * 100);
 
-        </div>}</>
-})
+        return (
+          <motion.div
+            key={item._id}
+            initial={{ opacity: 0, scale: 1, y: -30 }}
+            animate={{ y: 0, opacity: 1, scale: 1 }}
+            transition={{ delay: 0, duration: 0.3, stiffness: 50 }}
+            className='bg-white shadow-xl duration-200 hover:scale-105 cursor-pointer'
+          >
+            <Link href={`/product/${item._id}`}>
+              <Image
+                src={`/productIamages/${item.img}/thumbnail.jpg`}
+                alt="Product-Image"
+                width={400}
+                height={400}
+                className='transition-transform duration-300 hover:scale-105'
+              />
+              <div className='p-4 text-black-[0.9]'>
+                <h2 className='text-lg font-medium'>{item.title}</h2>
+                <div className='flex items-center text-black-[0.8]'>
+                  <p className='mr-2 text-lg font-semibold'>{item.price}$</p>
+                  <p className='text-base font-medium line-through'>{item.originalPrice}$</p>
+                  <p className='ml-auto text-green-300 font-base'>{discountPercentage}% off</p>
+                </div>
+              </div>
+            </Link>
+          </motion.div>
+        );
+      })}
+    </>}
+    </>
+  );
+};
 
-}
-                    </motion.div>
-    )
-}
-
+export default ProductCard;
