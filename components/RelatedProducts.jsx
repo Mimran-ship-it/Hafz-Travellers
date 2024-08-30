3a866b06fd8f271ac12408b03e2c1e65e195d662
Mimran-ship-it <mimran.ug22smme@gmail.com>
2024-08-31 00:30:38 +0500

    item=>packages

--- components/RelatedProducts.jsx
+++ components/RelatedProducts.jsx
@@ -24,7 +24,7 @@ const RelatedProducts = () => {
     
 <>
     <div className='mt-[50px] md:mt-[100px] mb-[100px] md:mb-0'>
-        <div className='text-2xl font-bold mb-5'>Relsted Items</div>
+        <div className='text-2xl font-bold mb-5'>Related Packages</div>
        
     </div>
 
